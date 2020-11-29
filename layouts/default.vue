afb08de1f7765d31518fec1341c196d72b16664d
Noemi Lemonnier <noemilemonnier@gmail.com>
2020-11-30 01:52:14 +0200

    button

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,9 +7,9 @@
         </div>
         <v-divider/>
         <v-list-item class="mt-1" link nuxt to="/"><v-icon color="grey darken-2" size="28px">{{ $i.mdiHomeVariant }}</v-icon >&emsp;Home page</v-list-item>
-        <v-list-item class="mt-1" link nuxt to="/jackets"><v-icon size="28px">{{ $i.mdiLogin }}</v-icon>&emsp;Jackets</v-list-item>
-        <v-list-item class="mt-1" link nuxt to="/shirts"><v-icon size="28px">{{ $i.mdiLogin }}</v-icon>&emsp;Shirts</v-list-item>
-        <v-list-item class="mt-1" link nuxt to="/accessories"><v-icon size="28px">{{ $i.mdiLogin }}</v-icon>&emsp;Accessories</v-list-item>
+        <v-list-item class="mt-1" link nuxt to="/jackets">&emsp;Jackets</v-list-item>
+        <v-list-item class="mt-1" link nuxt to="/shirts">&emsp;Shirts</v-list-item>
+        <v-list-item class="mt-1" link nuxt to="/accessories">&emsp;Accessories</v-list-item>
         <v-divider/>
       </v-list>
       <template v-slot:append>
@@ -20,10 +20,10 @@
     <v-app-bar app dense short>
       <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="sideNav = !sideNav"></v-app-bar-nav-icon>
         <v-toolbar-items class="hidden-sm-and-down">
-          <v-tab text nuxt to="/">Home page</v-tab>
-          <v-tab text nuxt to="/jackets">Jackets</v-tab>
-          <v-tab text nuxt to="/shirts">Shirts</v-tab>
-          <v-tab text nuxt to="/accessories">Accessories</v-tab>
+          <v-btn text nuxt to="/">Home page</v-btn>
+          <v-btn  text nuxt to="/jackets">Jackets</v-btn>
+          <v-btn text nuxt to="/shirts">Shirts</v-btn>
+          <v-btn text nuxt to="/accessories">Accessories</v-btn>
         </v-toolbar-items>
       <v-spacer> </v-spacer>
       <nuxt-link to="/" class="hidden-sm-and-down ml-5"><img src="/img/logo.png" contain width="45px" height="45px" alt="Aidline logo" /></nuxt-link>
